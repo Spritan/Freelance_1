8704efec182c7b13694ac6efa647e8b980ebd76c
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2021-07-06 17:13:33 +0530

    .

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+        // sticky navbart on scroll script
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
+            $('.navbart').addClass("sticky");
         }else{
-            $('.navbar').removeClass("sticky");
+            $('.navbart').removeClass("sticky");
         }
         
         // scroll-up button show/hide script
@@ -22,14 +22,14 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbart .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
+    // toggle menu/navbart script
     $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
+        $('.navbart .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
