ca7eb2b02d05721b17f907222690c42c5cfbf0c9
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2021-07-12 21:04:02 +0530

    .

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
         // sticky navbart on scroll script
         if(this.scrollY > 20){
             $('.navbart').addClass("sticky");
+            $('.navbart2').addClass("sticky");
         }else{
             $('.navbart').removeClass("sticky");
+            $('.navbart2').removeClass("sticky");
         }
-        
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
@@ -14,7 +15,6 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
@@ -26,10 +26,16 @@ $(document).ready(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
+    $('.navbart2 .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
     // toggle menu/navbart script
     $('.menu-btn').click(function(){
+        $('.navbart2 .menu').toggleClass("active");
         $('.navbart .menu').toggleClass("active");
+
         $('.menu-btn i').toggleClass("active");
     });
 
